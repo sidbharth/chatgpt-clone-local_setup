3c78133c519f40f9aead6e4be5eb18f577f0db17
Nada <nadaaofficial19@gmail.com>
2023-05-29 18:18:52 +0530

    login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import Home from './pages/Home'
-import { ChatContextProvider } from './context/chatContext'
-import { useEffect, useState } from 'react'
-import Loading from './components/Loader'
+import Home from "./pages/Home";
+import { ChatContextProvider } from "./context/chatContext";
+import { useEffect, useState } from "react";
+import Loading from "./components/Loader";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
 
 const App = () => {
-  const [loading , setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setInterval(() => {
-        setLoading(false)
-    }, 2000)
-}, [])
+      setLoading(false);
+    }, 2000);
+  }, []);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: loading ? <Loading /> : <Home />,
+    },
+    { path: "/login", element: <Login /> },
+  ]);
   return (
     <ChatContextProvider>
-      <div>
-       {loading ? <Loading />: <Home />}
-      </div>
-    </ChatContextProvider >
-  )
-}
+      <RouterProvider router={router} />
+    </ChatContextProvider>
+  );
+};
 
-
-export default App
+export default App;
